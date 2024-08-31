8bb5916c198c23ceacb65560d8150dc2148de6be
bala160 <krishjo160@gmail.com>
2024-08-31 15:59:29 +0000

    Sample program

---
+++ sample.java
@@ -0,0 +1,9 @@
+public class sample {
+    public static void main(String[] args) {
+        String[] data = {"Bala","Datastructure"};
+
+        for(String result : data){
+            System.out.println(result);
+        }
+    }
+}
